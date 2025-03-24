a08cce968e412b1ba84b8c265e3bda6f35b7fed6
Guilherme Nóbrega <gbn6699@gmail.com>
2025-03-24 14:00:17 -0300

    django admin

--- employee_directory/settings.py
+++ employee_directory/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employees',
 ]
 
 MIDDLEWARE = [
@@ -126,4 +127,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-####################
+# Media files configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
